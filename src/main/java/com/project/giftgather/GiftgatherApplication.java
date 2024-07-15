b0772bb8b2ed396ae9203c2491ea4a156ab4d18a
hyeOOO <hyewon5342@nate.com>
2024-07-15 23:18:14 +0900

    first commit

---
+++ src/main/java/com/project/giftgather/GiftgatherApplication.java
@@ -0,0 +1,13 @@
+package com.project.giftgather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftgatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftgatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/giftgather/GiftgatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.giftgather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftgatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
